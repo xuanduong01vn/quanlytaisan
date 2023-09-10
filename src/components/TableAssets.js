9295b98fa7410d7240be42fc23a2a5c1404e92dc
duongnx <duong.nx@zinza.com.vn>
2023-09-11 01:06:52 +0700

    Custom more data table layout

--- src/components/TableAssets.js
+++ src/components/TableAssets.js
@@ -19,31 +19,50 @@ function TableAsset(){
                         </select>
                         tài sản
                     </div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Mã tài sản</th>
-                                <th>Tên tài sản</th>
-                                <th>Trạng thái tài sản</th>
-                            </tr>
+                    <div className="data-table-grid">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Mã tài sản</th>
+                                    <th>Tên tài sản</th>
+                                    <th>Trạng thái tài sản</th>
+                                </tr>
 
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>#1</td>
-                                <td>MH122</td>
-                                <td>Màn hình 123</td>
-                                <td>Đang sử dụng</td>
-                            </tr>
-                            <tr>
-                                <td>#2</td>
-                                <td>MH145</td>
-                                <td>Màn hình 145</td>
-                                <td>Không sử dụng</td>
-                            </tr>
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>#1</td>
+                                    <td>MH122</td>
+                                    <td>Màn hình 123</td>
+                                    <td>Đang sử dụng</td>
+                                </tr>
+                                <tr>
+                                    <td>#2</td>
+                                    <td>MH145</td>
+                                    <td>Màn hình 145</td>
+                                    <td>Không sử dụng</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="pagination-container">
+                        <div className="pagination-page-detail">
+                            Màn hình thứ 1 đến 10 trong 23 màn hình
+                        </div>
+                        <div className="pagination-page-list">
+                            <ul class="pagination">
+                                <li><a href="#">&lt;</a></li>
+                                <li><a href="#">1</a></li>
+                                <li class="active"><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">4</a></li>
+                                <li><a href="#">5</a></li>
+                                <li><a href="#">&gt;</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </TableAssetContainer>
@@ -80,21 +99,38 @@ const TableAssetContainer = styled.div`
                 }
             }
 
-            table{
-                border-collapse: collapse;
-                width: 100%;
+            .data-table-grid{
+                height: 444px;
+                padding: 12px 0;
+                box-sizing: border-box;
 
-                tr{
-                    th, td{
-                        border: 1px solid lightgrey;
-                        padding: 8px 12px;
-                    } 
-                }
+                table{
+                    border-collapse: collapse;
+                    width: 100%;
 
-                tbody tr:nth-child(odd) {
-                        background-color: rgba(0, 0, 0, 0.06);
+                    tr{
+                        
+
+                        th, td{
+                            height: 38px;
+                            box-sizing: border-box;
+                            border: 1px solid lightgrey;
+                            padding: 8px 12px;
+                        } 
                     }
+
+                    tbody tr:nth-child(odd) {
+                            background-color: rgba(0, 0, 0, 0.06);
+                        }
+                }
             }
+
+            .pagination-container{
+                padding-top: 12px;
+                display: flex;
+                justify-content: space-between;
+            }
+
 
         }
 
